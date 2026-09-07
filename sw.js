8fb533281f4c0388445d19c4ad670ebc63a8ffbe
usorismortui <115591884+usorismortui@users.noreply.github.com>
2026-09-08 02:38:07 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shelf-v2';
+const CACHE = 'shelf-v4';
 const ASSETS = ['./', 'index.html', 'zxing.min.js', 'manifest.webmanifest'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
